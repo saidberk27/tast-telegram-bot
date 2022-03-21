7baf6699c65bb4d57a7be1cbf26a035d6d3b3c34
unknown <csaidberk@gmail.com>
2022-03-22 00:17:19 +0300

    Bot Creator Eklendi.

---
+++ TELEGRAM-INTERFACED/bot creator.py
@@ -0,0 +1,53 @@
+import os
+import json
+
+
+def createFolders(userName,botToken):
+    mainFolder = "{}".format(userName)
+    os.makedirs(mainFolder)
+
+    jobsFolder = mainFolder + "/jobs"
+    os.makedirs(jobsFolder)
+
+    mediasFolder = mainFolder + "/medias"
+    os.makedirs(mediasFolder)
+
+    userJsonDict = {"channel-data": {}, "post-data": {}, "folder-data": {}}
+    emptyUserJson = open(mainFolder + "/userJson.json","w")
+    emptyUserJson.write(json.dumps(userJsonDict))
+    emptyUserJson.close()
+    
+    activeWorksJsonDict = {}
+    emptyActiveWorksJson = open(mainFolder + "/active-works.json", "w")
+    emptyActiveWorksJson.write(json.dumps(activeWorksJsonDict))
+    emptyUserJson.close()
+
+    os.system("copy main.py {}".format(mainFolder))
+    os.system("copy STRINGS_EN.py {}".format(mainFolder))
+    os.system("copy STRINGS_HW.py {}".format(mainFolder))
+
+    userListPath = mainFolder + "/userList.txt"
+    userList = open(userListPath,"w")
+    userList.write(userName)
+    userList.close()
+
+    logsFilePath = mainFolder + "/logs.txt"
+    logsList = open(logsFilePath, "w")
+    logsList.write("BOT CREATED")
+    logsList.close()
+
+    startBatFile = mainFolder + "/start.bat"
+    logsList = open(startBatFile, "w")
+    logsList.write("python3 main.py")
+    logsList.close()
+
+    botTokenFile = mainFolder + "/botToken.txt"
+    botTokenWrite = open(botTokenFile, "w")
+    botTokenWrite.write(botToken)
+    botTokenWrite.close()
+
+if __name__ == '__main__':
+    userName = input("Please Type Username of User of This Bot: ")
+    botToken = input("Please Enter Bot Token: ")
+    print("DISCLAIMER! MAKE SURE YOUR BOT HAS BEEN ADDED TO EVERY GROUP YOU WANT TO PUBLISH ADVERTISEMENTS (BEING ADMIN IS NOT REQUIRED)")
+    createFolders(userName,botToken)
